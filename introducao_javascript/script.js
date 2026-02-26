e14a2cd108d6917400597fb7d9ec10b266fac058
joao pedro alves dos santos <joaopedroalvessantos@gmail.com>
2026-02-26 18:10:58 -0300

    learn: encerra codigo 1.4.1

--- introducao_javascript/script.js
+++ introducao_javascript/script.js
@@ -1,1 +1,73 @@
-console.log('Hello, JS!')
+/* //comparação por igualdade
+//Javascript == ou ===(usamos apenas esse)
+console.log(1 == 1)
+console.log(1 === 1)
+console.log(1 == '1')
+console.log(1 === '1') //false
+console.log(true == 1)
+console.log(1 == [1])
+console.log(1 === [1]) //false
+console.log(null == null)
+console.log(null == undefined)
+console.log([] == false)
+console.log([] == []) //false
+console.log([] === []) //false */
+
+/* //coerção explicita
+const n1 = 2
+const n2 = '3'
+const n3 = n1 + Number(n2)
+console.log(`${n1} + ${n2} = ${n3}`) */
+
+
+
+
+/* //coerção implicita
+const n1 = 2
+const n2 = '3'
+const n3 = n1 + n2
+console.log('${n1} + ${n2} = ${n3}') */
+
+
+
+
+//tipos
+//js é dinamicamente tipa
+/* let a = 2 
+console.log(typeof(a))
+a = true 
+console.log(typeof(a)) */
+
+
+
+
+
+//declaração de variaveis
+//cons, let e var
+//var idade = 18
+//console.log('Oi, ${nome}.')
+//if (idade >= 18){
+//    var nome = 'Maria'
+//    console.log(`Parabens, ${nome}. Voce pode dirigir.`)
+//}
+//console.log("Até mais, " + nome + '.')
+
+
+//var linguagem = 'Javascript'
+//console.log('Aprendendo ${linguagem}')
+//var linguagem = 'Java'
+//console.log('Aprendendo' + linguagem)
+
+
+//const nome = 'José'
+//console.log(nome)
+//nome = 'José Silva'
+//console.log(nome)
+
+//let nome = 'Maria'
+//console.log(nome)
+//nome = `meu nome é ${nome}.`
+//console.log(nome)
+
+
+//console.log('Hello, JS!')
