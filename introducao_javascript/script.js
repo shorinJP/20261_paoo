d11e7a7ab91e2ba5a104e818354f88fedcdf2eb2
joao pedro alves dos santos <joaopedroalvessantos@gmail.com>
2026-03-05 18:16:23 -0300

    learn: Encerra o capitulo 2

--- introducao_javascript/script.js
+++ introducao_javascript/script.js
@@ -1,4 +1,62 @@
-//closure
+//objetos Javascript
+let calculadora = {
+    somar: (a,b) => a + b,
+    subtrair: function(a,b){return a - b}
+}
+
+let res1 = calcularoda.somar(1, 2)
+console.log(res1)
+console.log(calculadora.subtrair(2, 1))
+console.log(calculadora['subtrair'](2, 1))
+
+/* //uma concessionario tem cnpj e um endereço. Ela possui alguns carros em estoque. cada carro tem
+//marca, modelo e ano de fabricação
+let concessionaria = {
+    CNPJ: '18.715.383/0001-40',
+    endereco: {
+        logradouro: 'Avenida Afonso Pena',
+        numero: 1212,
+        bairro: 'Centro, BH'
+    },
+    carros: [
+        {marca: "Fiat",  modelo: "Siena",  ano: 2008, proprietarios: [
+            {
+                nome: 'João', telefone: 65656565
+            },
+            {
+                nome: 'Rodrigo', telefone: 5555555
+            }
+        ]},
+        {marca: "Ford",  modelo: "Fusion",  ano: 2015, proprietarios: 0},
+        {marca: "Chevrolet",  modelo: "Onix",  ano: 2025, proprietarios: 0},
+        {marca: "Volkswagen",  modelo: "Golf GT",  ano: 2018, proprietarios: 0}
+    ]
+} */
+
+/* //uma pessoa se chama Maria, tem 21 anos e mora na Rua B, numero 20
+let pessoa ={
+    nome: 'Maria',
+    idade: 21,
+    endereco: {
+        logradouro: 'Rua B',
+        numero: 20,
+        bairro: 'Vila J'
+    }
+}
+console.log(`${pessoa.nome} mora na ${pessoa.endereco.logradouro}.`)
+console.log(`${pessoa['nome']} tem ${pessoa['idade']} anos. Ela mora na ${pessoa.endereco['logradouro']}, numero ${pessoa['endereco'].numero}.`) */
+
+
+/* //uma pessooa cujo o nome é joão e tem 17 anos
+let pessoa = {
+    nome: 'João',
+    ideda: 17
+} */
+
+
+
+
+/* //closure
 function eAgora(){
     let cont = 1
     function f1(){
@@ -13,7 +71,7 @@ function eAgora(){
 }
 let res = eAgora()
 res.f1()
-res.f2()
+res.f2() */
 
 /* function saudacoesFactory(saudacao, nome){
     let outra 
